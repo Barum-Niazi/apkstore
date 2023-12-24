6180701192de6a551336765fb2dd8bc0d8b250bc
Barum Niazi <96373839+Barum-Niazi@users.noreply.github.com>
2023-12-24 20:18:42 +0500

    Update appinfo.dart

--- lib/appinfo.dart
+++ lib/appinfo.dart
@@ -1,23 +1,33 @@
-class AppInfo {
-  final int id;
-  final String name;
-  final String image1Url;
-  final String image2Url;
-  final String image3Url;
-  final String image4Url; // Added image4Url
-  final String instagramDescription;
-  final String category;
-  final String downloadLink;
-  final String size;
-  final double rating;
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
 
-  AppInfo({
+import 'dart:convert';
+
+Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+
+String welcomeToJson(Welcome data) => json.encode(data.toJson());
+
+class Welcome {
+  int id;
+  String name;
+  String image1Url;
+  String image2Url;
+  String image3Url;
+  String image4Url;
+  String instagramDescription;
+  String category;
+  String downloadLink;
+  String size;
+  double rating;
+
+  Welcome({
     required this.id,
     required this.name,
     required this.image1Url,
     required this.image2Url,
     required this.image3Url,
-    required this.image4Url, // Added image4Url
+    required this.image4Url,
     required this.instagramDescription,
     required this.category,
     required this.downloadLink,
@@ -25,19 +35,31 @@ class AppInfo {
     required this.rating,
   });
 
-  factory AppInfo.fromJson(Map<String, dynamic> json) {
-    return AppInfo(
-      id: json['id'],
-      name: json['name'],
-      image1Url: json['image1Url'],
-      image2Url: json['image2Url'],
-      image3Url: json['image3Url'],
-      image4Url: json['image4Url'], // Added image4Url
-      instagramDescription: json['instagramDescription'],
-      category: json['category'],
-      downloadLink: json['downloadLink'],
-      size: json['size'],
-      rating: json['rating'].toDouble(),
-    );
-  }
+  factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+        id: json["id"],
+        name: json["name"],
+        image1Url: json["image1Url"],
+        image2Url: json["image2Url"],
+        image3Url: json["image3Url"],
+        image4Url: json["image4Url"],
+        instagramDescription: json["instagramDescription"],
+        category: json["category"],
+        downloadLink: json["downloadLink"],
+        size: json["size"],
+        rating: json["rating"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "image1Url": image1Url,
+        "image2Url": image2Url,
+        "image3Url": image3Url,
+        "image4Url": image4Url,
+        "instagramDescription": instagramDescription,
+        "category": category,
+        "downloadLink": downloadLink,
+        "size": size,
+        "rating": rating,
+      };
 }

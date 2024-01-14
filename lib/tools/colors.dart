26acf34759ae4ef63a9a91f1fb83f2eedeb1f034
ANASASIF <anasasif118@gmail.com>
2024-01-14 23:54:44 +0500

    b

--- lib/tools/colors.dart
+++ lib/tools/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const backGroundColor1 = Colors.white;
-const backGroundColor2 = Color(0xFFE5E5E5);
+const backGroundColor1 = Colors.black;
+const backGroundColor2 = Colors.purple;
 const theme1 = Color(0xffb70808);
 const theme2 = Color(0xFFFFCDD2);
